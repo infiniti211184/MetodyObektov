73be89fc67755c5e6486cd3cb23bf9521559ac72
Julia Khramova <infiniti211184@mail.ru>
2022-09-14 20:30:25 +0300

    Дз методы объектов

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Book {
+    private final String name;
+    private final Author author;
+    private Integer publicationYear;
+
+
+    public Book(String name, Author author, Integer publicationYear) {
+        this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+}
+Footer
+        © 2022 GitHub, Inc.
+        Footer navigation
+        Terms
+        Privacy
+        Security
+        Status
+        Docs
+        Contact GitHub
+        Pricing
+        API
+        Training
+        Blog
+        About
